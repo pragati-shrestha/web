26bd87ba5926293a254f7ca6bdd0a233162f7e2c
Pragati Shrestha <pragati.shrestha07@gmail.com>
2024-10-17 16:08:55 +0545

    file added

---
+++ index.js
@@ -0,0 +1,10 @@
+//git init- to initialize the project          once initialized do't do it again
+//git status- to check the project status
+//git help or git -h
+//git add filename- to add a file
+//git add .- add all files
+//git commit-- project haru to add in local network
+//git config --global user.email "you@example.com"     --to override: write this again
+//git config --global user.name "Your Name"
+//git remote -v- view the remote origin
+happy 
